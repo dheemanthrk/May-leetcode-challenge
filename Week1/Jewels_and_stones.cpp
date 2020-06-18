78da5c73043d9798e9fd8efc2ea23e647d312ea6
dheemanthrk <dheemanthkumawat@gmail.com>
2020-06-18 11:25:39 +0530

    Ransom note

--- Week1/Jewels_and_stones.cpp
+++ Week1/Jewels_and_stones.cpp
@@ -35,21 +35,26 @@ int main()
 #include <iostream>
 using namespace std;
 
-int numJewelsInStones(string J, string S) {
-    int count = 0;
-    for(int jewel = 0; jewel<J.length(); jewel++){
-        for(int stone = 0; stone < S.length(); stone++){
-            if(J[jewel] == S[stone]){
-                count++;
-            }
-        }
+int numJewelsInStones(string J, string S)
+{
+  int count = 0;
+  for (int jewel = 0; jewel < J.length(); jewel++)
+  {
+    for (int stone = 0; stone < S.length(); stone++)
+    {
+      if (J[jewel] == S[stone])
+      {
+        count++;
+      }
     }
-    return count;
+  }
+  return count;
 }
 
-int main(){
-    string J = "aA";
-    string S = "aAAbbbb";
-    int count = numJewelsInStones(J, S);
-    cout << count << endl;
+int main()
+{
+  string J = "aA";
+  string S = "aAAbbbb";
+  int count = numJewelsInStones(J, S);
+  cout << count << endl;
 }
---
+++ Week1/Ransom_note.cpp
@@ -0,0 +1,7 @@
+class solution
+{
+public:
+    bool canConstruct(string r, string m)
+    {
+        }
+};
