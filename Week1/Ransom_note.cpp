85e2200c44eb16db7e77ee399812f676d63c0dec
dheemanthrk <dheemanthkumawat@gmail.com>
2020-06-18 21:41:02 +0530

    Ransom note solution

--- Week1/Ransom_note.cpp
+++ Week1/Ransom_note.cpp
@@ -1,7 +1,23 @@
-class solution
+class Solution
 {
 public:
-    bool canConstruct(string r, string m)
+    bool canConstruct(string ransomNote, string magazine)
     {
+        unordered_map<char, int> mp;
+        for (auto a : magazine)
+            mp[a]++;
+
+        for (auto a : ransomNote)
+        {
+            if (mp.find(a) == mp.end())
+                return false;
+            else
+            {
+                mp[a]--;
+                if (mp[a] == 0)
+                    mp.erase(a);
+            }
         }
+        return true;
+    }
 };
